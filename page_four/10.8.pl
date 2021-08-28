66b27f812864fb830a3b4ab2948032a7c260f501
nyq <johnstuart666@yahoo.com>
2021-08-28 18:15:19 +0800

    page_four

--- page_four/10.8.pl
+++ page_four/10.8.pl
@@ -2,13 +2,26 @@ use strict;
 use warnings;
 use File::Copy qw(copy cp);
 
-my $dir = "D:/TEMP/*.txt";
-my @files = glob($dir);
-foreach(@files){
-    print $_, ref($_), "\n";
-    # cp($_, "D:/TEMP/genes_copy.txt");
-    # cp($_, .) or die "fail to cp:$!";
-    cp($_,'D:/TEMP/genes_copy.txt');
+# my $dir = "D:/TEMP/*.txt";
+# my @files = glob($dir);
+# foreach(@files){
+#     print $_, ref($_), "\n";
+#     # cp($_, "D:/TEMP/genes_copy.txt");
+#     # cp($_, .) or die "fail to cp:$!";
+#     cp($_,"D:/TEMP");
+# }
+
+my $dir = "D:/TEMP/";
+opendir(DIR, $dir) or die "fail to open dir:$!";
+my @file = readdir(DIR);
+foreach(@file){
+    print $_, "\n";
+    if($_ eq "." || $_ eq ".."){
+
+    }else{
+        cp($_, $dir);
+    }
+    
 }
 
 
